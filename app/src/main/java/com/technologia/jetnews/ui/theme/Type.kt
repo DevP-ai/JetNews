3befe2028b9249b6fb12a686f87f40f224221274
Devajit <patar1447@gmail.com>
2024-10-25 11:34:57 +0530

    Logo, colors,theme and other drawable are setup

--- app/src/main/java/com/technologia/jetnews/ui/theme/Type.kt
+++ app/src/main/java/com/technologia/jetnews/ui/theme/Type.kt
@@ -2,10 +2,19 @@ package com.technologia.jetnews.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.technologia.jetnews.R
 
+val Poppins = FontFamily(
+    fonts = listOf(
+        Font(R.font.poppins_regular,FontWeight.Normal),
+        Font(R.font.poppins_bold,FontWeight.Bold),
+        Font(R.font.poppins_semibold,FontWeight.SemiBold)
+    )
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
@@ -14,7 +23,38 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+    bodySmall = TextStyle(
+        fontSize = 14.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 21.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontSize = 16.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 24.sp,
+    ),
+    displaySmall = TextStyle(
+        fontSize = 24.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 36.sp,
+    ),
+    displayMedium = TextStyle(
+        fontSize = 32.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 48.sp,
+    ),
+    labelSmall = TextStyle(
+        fontSize = 13.sp,
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        lineHeight = 19.sp,
+    ),
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +71,5 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+
 )
